96e3d16366a6c0d1d7aff70a3ddeeccfff7f397e
kennethwheee <kenneth.whee@gmail.com>
2026-08-01 13:57:36 +0900

    gd

--- functions/api/logout.js
+++
@@ -1,6 +0,0 @@
-import { json } from "../_shared/http.js";
-import { clearSessionCookie } from "../_shared/auth.js";
-
-export function onRequestPost() {
-  return json({ ok: true }, 200, { "Set-Cookie": clearSessionCookie(), "Cache-Control": "no-store" });
-}
